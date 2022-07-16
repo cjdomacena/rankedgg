0d0fa54a0f1f61ac530a2ea662f12e61672ea7ab
cjdomacena <cjdomacena@yahoo.com>
2022-07-15 23:42:30 -0400

    refactor: :recycle: Changed logo subtext

--- src/components/navigation/NavLogo.tsx
+++ src/components/navigation/NavLogo.tsx
@@ -8,7 +8,7 @@ const NavLogo = (props: Props) => {
       <DotaLogo className="w-10  h-10 fill-neutral-800 hover:fill-neutral-200 transition-all duration-500 hover:text-black" />
       <div>
         <h1 className="text-neutral-200 font-noto-sans font-black leading-none">Dota Buffer</h1>
-        <p className="text-xs text-neutral-400">More buffer than dotabuff</p>
+        <p className="text-xs text-neutral-400">Buffer than Fly`</p>
       </div>
     </div>
   );
