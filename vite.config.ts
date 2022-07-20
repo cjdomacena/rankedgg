56f434b6c151b369f65d0d8f838154058efedcc3
cjdomacena <cjdomacena@yahoo.com>
2022-07-19 21:48:07 -0400

    build: :package: Added vite-plugin-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {resolve} from 'path'
+import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'public/index.html'),
-      }
-    }
-  }
+  plugins: [react(), Pages()]
 })
