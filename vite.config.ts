3210654348f1e9d5b670a592d93586f4900bed52
cjdomacena <cjdomacena@yahoo.com>
2022-07-19 22:25:02 -0400

    fix: :construction: Fixing direct paste url not working

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Pages from 'vite-plugin-pages'
+import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), Pages()]
+  plugins: [react(), Pages()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'index.html')
+      }
+    }
+  }
 })
