a1052bf0857195e6f02763c07bd6aa827686accc
cjdomacena <cjdomacena@yahoo.com>
2022-08-01 17:40:46 -0400

    refactor: :recycle: Removed unused component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import PageHeaderBG from '../components/Header/PageHeaderBG'
 import PrimaryLayout from '../components/Layouts/PrimaryLayout'
-import Benchmark from '../components/Players/Benchmark';
 type Props = {}
 
 const Home = (props: Props) => {
@@ -12,8 +11,6 @@ const Home = (props: Props) => {
       <div className="w-full h-auto  grid grid-cols-12">
       <div className='h-full'></div>
       </div>
-
-      <Benchmark />
     </PrimaryLayout>
   );
 }
