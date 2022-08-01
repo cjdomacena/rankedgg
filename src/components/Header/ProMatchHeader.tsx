302c5f01ee9e3cdd8c3a83383a079d0b455db0da
cjdomacena <cjdomacena@yahoo.com>
2022-07-31 22:47:00 -0400

    refactor: :recycle: Code splitting

---
+++ src/components/Header/ProMatchHeader.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { formatDuration, formatStartTime } from '../../../utils';
+import { GAME_MODES } from '../../../utils/constants';
+
+type Props = {
+	matchId: number,
+	matchDuration:number,
+	gameMode:number,
+	leagueName:string,
+	tier: string,
+	startTime:number
+}
+
+const ProMatchHeader = ({matchId, matchDuration, gameMode, leagueName, tier, startTime}: Props) => {
+  return (
+    <div className="container mx-auto px-8 py-4 gap-2 flex justify-between items-center">
+      <div>
+        <h4 className="text-sm font-semibold">{matchId} </h4>
+        <p className="text-sm">{formatDuration(matchDuration)}</p>
+        <p className="text-sm capitalize">{GAME_MODES[gameMode].name.split("_").join(" ")}</p>
+      </div>
+      <div className="text-right">
+        <p className="text-sm">{leagueName}</p>
+        <p className="text-sm capitalize">{tier}</p>
+        <p className="text-sm">{formatStartTime(startTime, matchDuration)}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ProMatchHeader
