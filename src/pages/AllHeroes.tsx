21bd56b528935645d2449fbb9be93ed41f5a5419
cjdomacena <cjdomacena@yahoo.com>
2022-07-31 23:01:40 -0400

    refactor: :recycle: Updated loading state

--- src/pages/AllHeroes.tsx
+++ src/pages/AllHeroes.tsx
@@ -8,13 +8,14 @@ import { FiGrid } from "react-icons/fi";
 import PrimaryLayout from "../components/Layouts/PrimaryLayout";
 import PageHeader from "../components/Header/PageHeader";
 import PageHeaderBG from "../components/Header/PageHeaderBG";
+import ErrorComponent from "../components/Error";
 const AllHeroes: React.FC = () => {
-  const { status, data } = useHeroes();
+  const { status, data, isError } = useHeroes();
   const id = useId();
-  const placeHolderItems = [0, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12];
+
   const [searchInput, setSearch] = useState<string>("");
   const [matches, setMatches] = useState<string[]>(HERO_LIST);
-  const [isSearching, setIsSearching] = useState<boolean>(false);
+
   const handleSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const q = e.target.value;
     if (q.length === 0) {
@@ -22,115 +23,102 @@ const AllHeroes: React.FC = () => {
     }
     setSearch(q);
     if (q.length > 0) {
-      setIsSearching(true);
       const partialMatches = filterSearchHero(q, HERO_LIST);
       setMatches(partialMatches);
     }
   };
 
-  if (status === "loading") {
-    return (
-      <PrimaryLayout>
-        <div className="container mx-auto font-bold flex justify-between text-neutral-300 items-center p-4 flex-wrap gap-2">
-          <PageHeaderBG>
+  if (isError) {
+    return <ErrorComponent />;
+  }
+
+  switch (status) {
+    case "success": {
+      return (
+        <PrimaryLayout className="h-full flex items-center min-h-screen">
+          <div className="w-full h-full">
+            <PageHeaderBG>
+              <div className="container mx-auto flex justify-between text-neutral-300 items-center pt-4 px-12 flex-wrap gap-2">
+                <PageHeader icon={<FiGrid className="w-6 h-6 mr-2" />} title="All Heroes" />
+                <input
+                  className=" input input-sm ring ring-neutral rounded xl:w-auto lg:w-auto md:w-auto w-full"
+                  placeholder="Search Hero"
+                  value={searchInput}
+                  onChange={handleSearchInput}
+                />
+              </div>
+            </PageHeaderBG>
+            <div className="container mx-auto pt-8 pb-10 px-8">
+              <AllHeroLayout type="Strength">
+                {data.strength.map((hero) => (
+                  <HeroImage
+                    src={hero.img}
+                    key={`${id}-${hero.id}`}
+                    className={
+                      matches.includes(formatHeroName(hero.localized_name))
+                        ? formatHeroName(hero.localized_name)
+                        : "opacity-30"
+                    }
+                    heroName={formatHeroName(hero.localized_name)}
+                    id={hero.id}
+                  />
+                ))}
+              </AllHeroLayout>
+              <AllHeroLayout type="Agility">
+                {data.agility.map((hero) => (
+                  <HeroImage
+                    src={hero.img}
+                    key={`${id}-${hero.id}`}
+                    className={
+                      matches.includes(formatHeroName(hero.localized_name))
+                        ? "opacity-100" //formatHeroName(hero.localized_name)
+                        : "opacity-30"
+                    }
+                    heroName={formatHeroName(hero.localized_name)}
+                    id={hero.id}
+                  />
+                ))}
+              </AllHeroLayout>
+              <AllHeroLayout type="Intelligence">
+                {data.intelligence.map((hero) => (
+                  <HeroImage
+                    src={hero.img}
+                    key={`${id}-${hero.id}`}
+                    className={
+                      matches.includes(formatHeroName(hero.localized_name))
+                        ? "opacity-100" //formatHeroName(hero.localized_name)
+                        : "opacity-30"
+                    }
+                    heroName={formatHeroName(hero.localized_name)}
+                    id={hero.id}
+                  />
+                ))}
+              </AllHeroLayout>
+            </div>
+          </div>
+        </PrimaryLayout>
+      );
+    }
+    default: {
+      return (
+        <PrimaryLayout>
+          <div className="container mx-auto font-bold flex justify-between text-neutral-300 items-center p-4 flex-wrap gap-2 h-full mt-12">
+            <PageHeaderBG />
             <PageHeader icon={<FiGrid className="w-6 h-6 mr-2" />} title="All Heroes" />
             <input
               className=" input input-md input-secondary rounded-sm xl:w-auto lg:w-auto md:w-auto w-full"
               placeholder="Search Hero"
-              value={searchInput}
-              onChange={handleSearchInput}
-              onFocus={() => setIsSearching((prev) => !prev)}
               disabled={true}
             />
-          </PageHeaderBG>
-        </div>
-        <div className="container mx-auto ">
-          <AllHeroLayout type="Strength">
-            {placeHolderItems.map((item) => (
-              <HeroImage key={`${id}-${item}-strength`} src={null} className="bg-gray-600" />
-            ))}
-          </AllHeroLayout>
-          <AllHeroLayout type="Agility">
-            {placeHolderItems.map((item) => (
-              <HeroImage key={`${id}-${item}-agility`} src={null} className="bg-gray-600" />
-            ))}
-          </AllHeroLayout>
-          <AllHeroLayout type="Intelligence">
-            {placeHolderItems.map((item) => (
-              <HeroImage key={`${id}-${item}-intelligence`} src={null} className="bg-gray-600" />
-            ))}
-          </AllHeroLayout>
-        </div>
-      </PrimaryLayout>
-    );
-  } else if (status === "success") {
-    return (
-      <PrimaryLayout className="h-full flex items-center">
-        <div className="w-full h-full">
-          <PageHeaderBG>
-            <div className="container mx-auto flex justify-between text-neutral-300 items-center pt-4 px-12 flex-wrap gap-2">
-              <PageHeader icon={<FiGrid className="w-6 h-6 mr-2" />} title="All Heroes" />
-              <input
-                className=" input input-sm ring ring-neutral rounded xl:w-auto lg:w-auto md:w-auto w-full"
-                placeholder="Search Hero"
-                value={searchInput}
-                onChange={handleSearchInput}
-                onFocus={() => setIsSearching((prev) => !prev)}
-              />
-            </div>
-          </PageHeaderBG>
-          <div className="container mx-auto pt-8 pb-10 px-8">
-            <AllHeroLayout type="Strength">
-              {data.strength.map((hero) => (
-                <HeroImage
-                  src={hero.img}
-                  key={`${id}-${hero.id}`}
-                  className={
-                    matches.includes(formatHeroName(hero.localized_name))
-                      ? formatHeroName(hero.localized_name)
-                      : "opacity-30"
-                  }
-                  heroName={formatHeroName(hero.localized_name)}
-                  id={hero.id}
-                />
-              ))}
-            </AllHeroLayout>
-            <AllHeroLayout type="Agility">
-              {data.agility.map((hero) => (
-                <HeroImage
-                  src={hero.img}
-                  key={`${id}-${hero.id}`}
-                  className={
-                    matches.includes(formatHeroName(hero.localized_name))
-                      ? "opacity-100" //formatHeroName(hero.localized_name)
-                      : "opacity-30"
-                  }
-                  heroName={formatHeroName(hero.localized_name)}
-                  id={hero.id}
-                />
-              ))}
-            </AllHeroLayout>
-            <AllHeroLayout type="Intelligence">
-              {data.intelligence.map((hero) => (
-                <HeroImage
-                  src={hero.img}
-                  key={`${id}-${hero.id}`}
-                  className={
-                    matches.includes(formatHeroName(hero.localized_name))
-                      ? "opacity-100" //formatHeroName(hero.localized_name)
-                      : "opacity-30"
-                  }
-                  heroName={formatHeroName(hero.localized_name)}
-                  id={hero.id}
-                />
-              ))}
-            </AllHeroLayout>
           </div>
-        </div>
-      </PrimaryLayout>
-    );
-  } else {
-    return <div>Oops something went wrong...</div>;
+          <div className="container mx-auto p-4 space-y-8">
+            <div className=" h-36 w-full bg-black/30 rounded animate-pulse"></div>
+            <div className=" h-36 w-full bg-black/30 rounded animate-pulse"></div>
+            <div className=" h-36 w-full bg-black/30 rounded animate-pulse"></div>
+          </div>
+        </PrimaryLayout>
+      );
+    }
   }
 };
 
