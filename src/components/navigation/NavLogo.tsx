695075e096aed5edb6da199043f24d2774aa37ea
cjdomacena <cjdomacena@yahoo.com>
2022-08-02 15:57:32 -0400

    fix: :bug: Updated import for logo

--- src/components/navigation/NavLogo.tsx
+++ src/components/navigation/NavLogo.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/LOGO.png"
 type Props = {};
 
 const NavLogo = (props: Props) => {
