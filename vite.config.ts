7e92d68a04e4a8b70b28eeb0c154ba95e8490461
cjdomacena <cjdomacena@yahoo.com>
2022-08-03 18:59:36 -0400

    build: :green_heart: vite config rollupotion

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import Pages from "vite-plugin-pages";
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), Pages()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id.toString().split("node_modules/")[1].split("/")[0].toString();
-          }
-        },
-      },
-    },
-  },
+
 });
